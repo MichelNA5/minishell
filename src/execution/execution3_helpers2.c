e2ae84ca100541c3c2027c9bd98e1ac03608b257
MichelNA5 <naous@LAPTOP-Q1MIFNRI.>
2026-01-13 02:28:07 +0200

    loop pipe exit code fixed

--- src/execution/execution3_helpers2.c
+++ src/execution/execution3_helpers2.c
@@ -12,6 +12,8 @@
 
 #include "minishell.h"
 
+#include <errno.h>
+
 void	handle_last_child_exit(int status, t_shell *shell)
 {
 	if (WIFEXITED(status))
@@ -34,27 +36,43 @@ void	handle_signal_output(int sig, int *newline_printed)
 	}
 }
 
+static void	update_last_signal(int status, int *last_signal,
+				int *newline_printed)
+{
+	int	sig;
+
+	if (!WIFSIGNALED(status))
+		return ;
+	sig = WTERMSIG(status);
+	*last_signal = sig;
+	handle_signal_output(sig, newline_printed);
+}
+
 void	wait_all_children(t_parser *parser, t_shell *shell, pid_t *pids)
 {
 	int	i;
 	int	status;
-	int	sig;
 	int	newline_printed;
+	int	last_signal;
 
 	i = 0;
 	newline_printed = 0;
+	last_signal = 0;
 	while (i < parser->cmd_count)
 	{
-		waitpid(pids[i], &status, 0);
-		if (i == parser->cmd_count - 1)
-			handle_last_child_exit(status, shell);
-		if (WIFSIGNALED(status))
+		if (waitpid(pids[i], &status, 0) == -1)
 		{
-			sig = WTERMSIG(status);
-			handle_signal_output(sig, &newline_printed);
+			if (errno == EINTR)
+				continue ;
+			break ;
 		}
+		update_last_signal(status, &last_signal, &newline_printed);
 		i++;
 	}
+	if (last_signal != 0)
+		shell->exit_status = 128 + last_signal;
+	else
+		handle_last_child_exit(status, shell);
 }
 
 void	execute_pipeline(t_parser *parser, t_shell *shell)
